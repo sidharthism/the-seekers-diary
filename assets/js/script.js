9cd2038748aeab1f4788f7bea5e8a99c7b59f6f7
Sidharth S <siddhuinchakattu@gmail.com>
2020-10-16 02:59:14 +0530

    Initial

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$("#flipbook").turn({
+    width: 960,
+    height: 480,
+    autoCenter: false,
+});
+
+var css1="font-size: 19px;";
+var css2="font-size: 19px; color: red";
+console.log("%cMade with "+"%c❤"+"%c by @sidharthism", css1, css2, css1);
